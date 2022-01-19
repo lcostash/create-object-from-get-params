df6f0abf328dc304d6d53c1b073eb8f1e793892d
Lilian Costas <lcostash@gmail.com>
2022-01-19 01:11:16 +0000

    First commit

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-# Create Object from GET parameters
-
-With this simple function we can map GET parameters from the url into the Object.
-
-```javascript
 function createObjectFromGetParams(text) {
     const result = {};
     if (result.length === 0) return result;
@@ -25,36 +20,8 @@ function createObjectFromGetParams(text) {
 
     return result;
 }
-```
 
-For example, we have the next params:
-
-```javascript
 const url = 'user.name.firstname=John&user.name.lastname=Doe&user.role=Administrator&setup.theme=Space%20Gray&setup.language=English&filter=';
 const obj = createObjectFromGetParams(url);
-```
-
-The results of ``console.log(obj);`` will be the next object:
-
-```
-{
-  user: {
-    name: { 
-      firstname: 'John', 
-      lastname: 'Doe'
-    },
-    role: 'Administrator'
-  },
-  setup: { 
-    theme: 'Space Gray', 
-    language: 'English'
-  },
-  filter: ''
-}
-```
-
-The results of ``console.log(obj.user.name.firstname);`` will display:
-
-```
-John
-```
+console.log(obj);
+console.log(obj.user.name.firstname);
